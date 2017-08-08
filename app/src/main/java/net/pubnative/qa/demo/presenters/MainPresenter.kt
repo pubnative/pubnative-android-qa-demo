054cc07698c44ed2ebf22dcdad68ce7acc86e1ac
Artsiom Aniskou <rrhack@gmail.com>
2017-08-08 12:22:13 +0200

    Add hide config container if apptoken is empty

--- app/src/main/java/net/pubnative/qa/demo/presenters/MainPresenter.kt
+++ app/src/main/java/net/pubnative/qa/demo/presenters/MainPresenter.kt
@@ -6,7 +6,6 @@ import net.pubnative.qa.demo.views.BaseView
 import net.pubnative.qa.demo.views.MainView
 import net.pubnative.sdk.core.Pubnative
 import net.pubnative.sdk.core.config.PNConfigManager
-import net.pubnative.sdk.core.config.model.PNConfigModel
 import net.pubnative.sdk.core.request.PNAdTargetingModel
 
 class MainPresenter : BasePresenter<BaseView>() {
@@ -25,6 +24,7 @@ class MainPresenter : BasePresenter<BaseView>() {
         if (appToken.isEmpty()) {
             view()?.hideIndicator()
             view()?.showErrorMessage(Exception("App Token is empty!"))
+            (view() as MainView).hideConfigs()
         } else {
             mAppToken = appToken
             PNConfigManager.getConfig(view()?.getContext(), appToken) {  model ->
